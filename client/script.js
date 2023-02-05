9ab4bd59cb3c83e4a08fe674b4b90023a18e0324
Ritik Bhandari <103035081+Ritik-Bhandari@users.noreply.github.com>
2023-02-05 12:48:40 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://karen-sb7u.onrender.com/', {
+    const response = await fetch('https://karen-npvh.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
